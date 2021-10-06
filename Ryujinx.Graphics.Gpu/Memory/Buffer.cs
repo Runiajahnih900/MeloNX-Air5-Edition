Runiajahnih900/MeloNX-Air5-Edition

commit f1d0ddaef54d1edba453e303190e4de3a3ad0239

Smaller initial size for ModifiedRangeList & directly inherit range list (#2663)
This fixes a potential regression with the new range list changes, where the cost for creating new ones would be rather large due to creating a 1024 size array. Also reduces cost for range list inheritance by using the first existing range list as a base, rather than creating a new one then adding both lists to it.

The growth size for the RangeList is now identical to its initial size. Every 32 elements was probably a little too common - now it is 1024 for most things and 8 for the buffer modified range list.

The Unmapped and SyncMethod methods have been changed to ensure that they behave properly if the range list is set null. Cleaned up a few calls to use the null-conditional operator.

Ryujinx.Graphics.Gpu/Memory/Buffer.cs
@@ -259,10 +259,7 @@ namespace Ryujinx.Graphics.Gpu.Memory
         /// <param name="size">The size of the region</param>
         public void ClearModified(ulong address, ulong size)
         {
-            if (_modifiedRanges != null)
-            {
-                _modifiedRanges.Clear(address, size);
-            }
+            _modifiedRanges?.Clear(address, size);
         }
 
         /// <summary>
@@ -275,7 +272,7 @@ namespace Ryujinx.Graphics.Gpu.Memory
 
             if (_useGranular)
             {
-                _modifiedRanges.GetRanges(Address, Size, (address, size) =>
+                _modifiedRanges?.GetRanges(Address, Size, (address, size) =>
                 {
                     _memoryTrackingGranular.RegisterAction(address, size, _externalFlushDelegate);
                     SynchronizeMemory(address, size);
@@ -302,18 +299,25 @@ namespace Ryujinx.Graphics.Gpu.Memory
                     _syncActionRegistered = true;
                 }
 
-                EnsureRangeList();
-                _modifiedRanges.InheritRanges(from._modifiedRanges, (ulong address, ulong size) =>
+                if (_modifiedRanges == null)
                 {
-                    if (_useGranular)
+                    _modifiedRanges = from._modifiedRanges;
+                    from._modifiedRanges = null;
+                }
+                else
+                {
+                    _modifiedRanges.InheritRanges(from._modifiedRanges, (ulong address, ulong size) =>
                     {
-                        _memoryTrackingGranular.RegisterAction(address, size, _externalFlushDelegate);
-                    }
-                    else
-                    {
-                        _memoryTracking.RegisterAction(_externalFlushDelegate);
-                    }
-                });
+                        if (_useGranular)
+                        {
+                            _memoryTrackingGranular.RegisterAction(address, size, _externalFlushDelegate);
+                        }
+                        else
+                        {
+                            _memoryTracking.RegisterAction(_externalFlushDelegate);
+                        }
+                    });
+                }
             }
         }
 
@@ -381,10 +385,7 @@ namespace Ryujinx.Graphics.Gpu.Memory
         /// <param name="mSize">Size of the region to force dirty</param>
         public void ForceDirty(ulong mAddress, ulong mSize)
         {
-            if (_modifiedRanges != null)
-            {
-                _modifiedRanges.Clear(mAddress, mSize);
-            }
+            _modifiedRanges?.Clear(mAddress, mSize);
 
             if (_useGranular)
             {
@@ -497,7 +498,9 @@ namespace Ryujinx.Graphics.Gpu.Memory
         /// <param name="size">Size of the unmapped region</param>
         public void Unmapped(ulong address, ulong size)
         {
-            _modifiedRanges?.Clear(address, size);
+            BufferModifiedRangeList modifiedRanges = _modifiedRanges;
+
+            modifiedRanges?.Clear(address, size);
 
             UnmappedSequence++;
         }

Ryujinx.Graphics.Gpu/Memory/BufferModifiedRangeList.cs
@@ -60,6 +60,8 @@ namespace Ryujinx.Graphics.Gpu.Memory
     /// </summary>
     class BufferModifiedRangeList : RangeList<BufferModifiedRange>
     {
+        private const int BackingInitialSize = 8;
+
         private GpuContext _context;
 
         private object _lock = new object();
@@ -68,7 +70,7 @@ namespace Ryujinx.Graphics.Gpu.Memory
         /// Creates a new instance of a modified range list.
         /// </summary>
         /// <param name="context">GPU context that the buffer range list belongs to</param>
-        public BufferModifiedRangeList(GpuContext context)
+        public BufferModifiedRangeList(GpuContext context) : base(BackingInitialSize)
         {
             _context = context;
         }

Ryujinx.Memory/Range/RangeList.cs
@@ -33,18 +33,22 @@ namespace Ryujinx.Memory.Range
             }
         }
 
+        private const int BackingInitialSize = 1024;
         private const int ArrayGrowthSize = 32;
-        private const int BackingGrowthSize = 1024;
 
         private RangeItem<T>[] _items;
+        private readonly int _backingGrowthSize;
+
         public int Count { get; protected set; }
 
         /// <summary>
         /// Creates a new range list.
         /// </summary>
-        public RangeList()
+        /// <param name="backingInitialSize">The initial size of the backing array</param>
+        public RangeList(int backingInitialSize = BackingInitialSize)
         {
-            _items = new RangeItem<T>[BackingGrowthSize];
+            _backingGrowthSize = backingInitialSize;
+            _items = new RangeItem<T>[backingInitialSize];
         }
 
         /// <summary>
@@ -68,7 +72,7 @@ namespace Ryujinx.Memory.Range
         {
             if (Count + 1 > _items.Length)
             {
-                Array.Resize(ref _items, _items.Length + ArrayGrowthSize);
+                Array.Resize(ref _items, _items.Length + _backingGrowthSize);
             }
 
             if (index >= Count)

commit 263794ae5a91c1e6bdc34c34cf23f9f77a43049f

Reregister flush actions when taking a buffer's modified range list.
Fixes a regression from #2663 where buffer flush would not happen after a resize. Specifically caused the world map in Yoshi's Crafted World to flash.

I have other planned changes to this class so this might change soon, but this regression could affect a lot so it couldn't wait.

Ryujinx.Graphics.Gpu/Memory/Buffer.cs
@@ -299,24 +299,28 @@ namespace Ryujinx.Graphics.Gpu.Memory
                     _syncActionRegistered = true;
                 }
 
+                Action<ulong, ulong> registerRangeAction = (ulong address, ulong size) =>
+                {
+                    if (_useGranular)
+                    {
+                        _memoryTrackingGranular.RegisterAction(address, size, _externalFlushDelegate);
+                    }
+                    else
+                    {
+                        _memoryTracking.RegisterAction(_externalFlushDelegate);
+                    }
+                };
+
                 if (_modifiedRanges == null)
                 {
                     _modifiedRanges = from._modifiedRanges;
+                    _modifiedRanges.ReregisterRanges(registerRangeAction);
+
                     from._modifiedRanges = null;
                 }
                 else
                 {
-                    _modifiedRanges.InheritRanges(from._modifiedRanges, (ulong address, ulong size) =>
-                    {
-                        if (_useGranular)
-                        {
-                            _memoryTrackingGranular.RegisterAction(address, size, _externalFlushDelegate);
-                        }
-                        else
-                        {
-                            _memoryTracking.RegisterAction(_externalFlushDelegate);
-                        }
-                    });
+                    _modifiedRanges.InheritRanges(from._modifiedRanges, registerRangeAction);
                 }
             }
         }

Ryujinx.Graphics.Gpu/Memory/BufferModifiedRangeList.cs
@@ -318,6 +318,22 @@ namespace Ryujinx.Graphics.Gpu.Memory
             }
         }
 
+        /// <summary>
+        /// Calls the given action for modified ranges that aren't from the current sync number.
+        /// </summary>
+        /// <param name="rangeAction">The action to call for each modified range</param>
+        public void ReregisterRanges(Action<ulong, ulong> rangeAction)
+        {
+            ulong currentSync = _context.SyncNumber;
+            foreach (BufferModifiedRange range in this)
+            {
+                if (range.SyncNumber != currentSync)
+                {
+                    rangeAction(range.Address, range.Size);
+                }
+            }
+        }
+
         private void ClearPart(BufferModifiedRange overlap, ulong address, ulong endAddress)
         {
             Remove(overlap);